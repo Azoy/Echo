203264702ffde1ab07bcb29ec48d4bdd528f75d0
Azoy <aalonso128@outlook.com>
2020-03-15 11:04:31 -0400

    add some named keypaths too

--- Sources/EchoStoredProperties/StoredPropertyIterable.swift
+++ Sources/EchoStoredProperties/StoredPropertyIterable.swift
@@ -31,4 +31,28 @@ extension Reflection {
   public static func allKeyPaths<T>(for instance: T) -> [PartialKeyPath<T>] {
     allKeyPaths(for: type(of: instance))
   }
+  
+  public static func allNamedKeyPaths<T>(
+    for type: T.Type
+  ) -> [String: PartialKeyPath<T>] {
+    guard let metadata = reflect(type) as? TypeMetadata,
+          metadata.kind == .struct || metadata.kind == .class else {
+      return [:]
+    }
+    
+    var result = [String: PartialKeyPath<T>]()
+    
+    for i in 0 ..< metadata.contextDescriptor.fields.records.count {
+      let kp = createKeyPath(root: metadata, leaf: i) as! PartialKeyPath<T>
+      result[metadata.contextDescriptor.fields.records[i].name] = kp
+    }
+    
+    return result
+  }
+  
+  public static func allNamedKeyPaths<T>(
+    for instance: T
+  ) -> [String: PartialKeyPath<T>] {
+    allNamedKeyPaths(for: type(of: instance))
+  }
 }
